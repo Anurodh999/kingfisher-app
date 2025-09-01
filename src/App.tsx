d269e982a59bdc191879e827fffebdad9e3e19d4
Anurodh999 <955nikku@gmail.com>
2025-09-01 16:55:32 +0000

    app and css

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FlattenExample } from './components/FlattenExample';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+return (
+<div className="App">
+<h1>Flatten Object Demo</h1>
+<FlattenExample />
+</div>
+);
+};
 
 export default App;
